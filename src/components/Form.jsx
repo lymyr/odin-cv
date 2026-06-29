6d60de6d26acd2f85dc15bdde1bc1ef26f2c58ec
lymyr <176290600+lymyr@users.noreply.github.com>
2026-06-29 15:00:19 +0800

    feat: education and experience inputs now reflect on 'applicant' state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,7 @@ import "../styles/Form.css"
 import { useState } from "react";
 
 export default function Form({applicant, setApplicant}) {
+    console.log(applicant)
     function addEducation() {
         setApplicant({...applicant, education: [...applicant.education, {
               id: crypto.randomUUID(),
@@ -74,25 +75,31 @@ export default function Form({applicant, setApplicant}) {
             </GroupField>
 
             <GroupField text="Educational Experience">
-                {applicant.education.map(edu => {
+                {applicant.education.map((edu, i) => {
                     return (
                         <div className="input-wrapper" key={edu.id}>
                             {applicant.education.length > 1 && <button onClick={() => removeEdu(edu.id)}>X</button>}
                             <div>
                                 <Input
-                                label="School Name"
-                                id={`name-${edu.id}`}
-                                handleChange={
-                                    (e) => setApplicant({...applicant, name: e.target.value})
-                                }
+                                    label="School Name"
+                                    id={`name-${edu.id}`}
+                                    handleChange={
+                                        (e) => {
+                                            const newEdu = [...applicant.education];
+                                            newEdu[i] = {...newEdu[i], schoolName: e.target.value}
+                                            setApplicant({...applicant, education:newEdu})
+                                    }}
                                 >
                                 </Input>
                                 <Input
                                     label="Title of Study"
                                     id={`study-title-${edu.id}`}
                                     handleChange={
-                                        (e) => setApplicant({...applicant, email: e.target.value})
-                                    }
+                                        (e) => {
+                                            const newEdu = [...applicant.education];
+                                            newEdu[i] = {...newEdu[i], studyTitle: e.target.value}
+                                            setApplicant({...applicant, education:newEdu})
+                                    }}
                                 >
                                 </Input>
                                 <Input
@@ -100,8 +107,11 @@ export default function Form({applicant, setApplicant}) {
                                     id={`study-date-${edu.id}`}
                                     type="date"
                                     handleChange={
-                                        (e) => setApplicant({...applicant, phoneNo: e.target.value})
-                                    }
+                                        (e) => {
+                                            const newEdu = [...applicant.education];
+                                            newEdu[i] = {...newEdu[i], studyDate: e.target.value}
+                                            setApplicant({...applicant, education:newEdu})
+                                    }}
                                 >
                                 </Input>
                             </div>
@@ -115,20 +125,72 @@ export default function Form({applicant, setApplicant}) {
                 applicant.experience.length === 0 ? 
                 <AddButton text="Add Experience" handleClick={addExperience}></AddButton> :
                 <GroupField text="Practical Experience">
-                    {applicant.experience.map(exp => {
+                    {applicant.experience.map((exp, i) => {
                         return (
                             <div className="input-wrapper" key={exp.id}>
                                 {<button onClick={() => removeExp(exp.id)}>X</button>}
                                 <div>
-                                    <Input label="Company Name" id={`company-name-${exp.id}`}></Input>
-                                    <Input label="Position Title" id={`position-${exp.id}`}></Input>
+                                    <Input 
+                                        label="Company Name" 
+                                        id={`company-name-${exp.id}`}
+                                        handleChange={
+                                            (e) => {
+                                                const newExp = [...applicant.experience];
+                                                newExp[i] = {...newExp[i], companyName: e.target.value}
+                                                setApplicant({...applicant, experience:newExp})
+                                        }}
+                                    >
+                                    </Input>
+                                    <Input 
+                                        label="Position Title"
+                                        id={`position-${exp.id}`}
+                                        handleChange={
+                                            (e) => {
+                                                const newExp = [...applicant.experience];
+                                                newExp[i] = {...newExp[i], position: e.target.value}
+                                                setApplicant({...applicant, experience:newExp})
+                                        }}
+                                    >
+                                    </Input>
                                     <div className="input-label">
                                         <label htmlFor={`responsibilities-${exp.id}`}>Responsibilities</label>
-                                        <textarea wrap='soft' id={`responsibilities-${exp.id}`} placeholder="Separate using 'enter' or new line"></textarea>
+                                        <textarea 
+                                            wrap='soft' 
+                                            id={`responsibilities-${exp.id}`} 
+                                            placeholder="Separate using 'enter' or new line"
+                                            onChange={
+                                                (e) => {
+                                                    const newExp = [...applicant.experience];
+                                                    newExp[i] = {...newExp[i], responsibilities: e.target.value.split(`\n`)}
+                                                    setApplicant({...applicant, experience:newExp})
+                                            }}
+                                        >
+                                        </textarea>
                                     </div>
-                                    <Input label="From" id={`work-start-date-${exp.id}`} type="date"></Input>
-                                    <Input label="To" id={`work-end-date-${exp.id}`} type="date"></Input>
-                                    
+                                    <Input 
+                                        label="From" 
+                                        id={`work-start-date-${exp.id}`} 
+                                        type="date"
+                                        handleChange={
+                                            (e) => {
+                                                const newExp = [...applicant.experience];
+                                                newExp[i] = {...newExp[i], fromDate: e.target.value}
+                                                setApplicant({...applicant, experience:newExp})
+                                        }}
+                                    >
+                                    </Input>
+                                    <Input 
+                                        label="To"
+                                        id={`work-end-date-${exp.id}`}
+                                        type="date"
+                                        handleChange={
+                                            (e) => {
+                                                const newExp = [...applicant.experience];
+                                                newExp[i] = {...newExp[i], toDate: e.target.value}
+                                                setApplicant({...applicant, experience:newExp})
+                                        }}
+                                    >
+                                    </Input>
                                 </div>
                             </div>
                     )})}
