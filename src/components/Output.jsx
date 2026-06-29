13b1da997710cc15d3075ad68f10a945e61188e1
lymyr <176290600+lymyr@users.noreply.github.com>
2026-06-29 19:34:24 +0800

    style: made delete transform more subtle and fixed output for some edge cases

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -13,26 +13,36 @@ export default function Output({applicant}) {
                 <h2>{applicant.name}</h2>
                 <p>{applicant.email} {applicant.email != "" && applicant.phoneNo != "" ? "|" : ""} {applicant.phoneNo}</p>
             </div>
-            <div className="form-section">
-                <div className="form-title-wrapper">
-                    <h3>Educational Experience</h3>
-                </div>
-                <div className="form-description-wrapper">
-                    {applicant.education.map(edu => {
-                        return (
-                            <div className="form-description-section" key={edu.id}>
-                            <div>
-                                <h4>{edu.schoolName}</h4>
-                                <p>{edu.studyTitle}</p>
-                            </div>
-                            <p>{formatDate(edu.studyDate, "MMM yyyy")}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+
             {
-                applicant.experience.length > 0 &&
+                (applicant.education[0].schoolName != "" || applicant.education[0].studyTitle != "" || applicant.education[0].studyDate != "") &&
+                <div className="form-section">
+                    <div className="form-title-wrapper">
+                        <h3>Educational Experience</h3>
+                    </div>
+                    <div className="form-description-wrapper">
+                        {applicant.education.map(edu => {
+                            return (
+                                <div className="form-description-section" key={edu.id}>
+                                <div>
+                                    <h4>{edu.schoolName}</h4>
+                                    <p>{edu.studyTitle}</p>
+                                </div>
+                                <p>{formatDate(edu.studyDate, "MMM yyyy")}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            }
+            
+            {
+                applicant.experience.length > 0 && 
+                (
+                    applicant.experience[0].companyName != "" || applicant.experience[0].position != "" || 
+                    applicant.experience[0].responsibilities != "" || applicant.experience[0].fromDate != "" || 
+                    applicant.experience[0].toDate != ""
+                ) &&
                 <div className="form-section">
                     <div className="form-title-wrapper">
                         <h3>Professional Experience</h3>
